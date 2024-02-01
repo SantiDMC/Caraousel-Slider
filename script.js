76d1b77f0d0dc452aca13123a1aac1dd4cf27c03
santidmc127 <santiagoelias128@gmail.com>
2024-02-01 07:30:18 -0500

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 150,
+    modifier: 2.5,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
